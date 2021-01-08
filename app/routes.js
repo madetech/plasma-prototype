a6602237a41ca47c2a630ab76f90fad9d05cdc6d
petecraven91 <petecraven@fastmail.com>
2021-01-08 10:02:24 +0000

    Initial push of plasma prototype

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,22 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// Branching example
+router.post('/submit-medium', function (req, res) {
+
+    // Make a variable and give it the value from 'know-nhs-number'
+    var medium = req.session.data['medium']
+  
+    // Check whether the variable matches a condition
+    if (medium == "postal"){
+      // Send user to next page
+      res.redirect('/postal-form')
+    }
+    else {
+      // Send user to ineligible page
+      res.redirect('/pharmacy-form')
+    }
+  
+  })
+
 module.exports = router;
